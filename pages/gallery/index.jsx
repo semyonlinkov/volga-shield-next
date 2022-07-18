c8912f6441b522ac97690c59c26b841ecf6446c5
тестовое_имя <@>
2022-07-18 21:30:07 +0400

    wanna pooooop

--- pages/gallery/index.jsx
+++ pages/gallery/index.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import Header from '../../components/Header/Header';
 import styles from './AboutUsPage.module.scss';
 import BurgerMenu from '../../components/BurgerMenu/BurgerMenu';
+import Image from 'next/image';
+
+import IMGAutoprobeg from '../../public/images/gallery/autoprobeg/013.jpg';
 
 const AboutUsPage = () => {
 	const [isBurgerActive, setIsBurgerActive] = useState(false);
@@ -14,9 +17,12 @@ const AboutUsPage = () => {
 				setIsBurgerActive={setIsBurgerActive}
 				style={{ backgroundColor: '#191919' }}
 			/>
-			<section className={styles.aboutUs_wrapper}>
-				<div className={styles.gallery}>
-					
+			<section className={styles.wrapper}>
+				<div className={styles.gallery_wrapper}>
+					<div>
+						<Image src={IMGAutoprobeg} />
+						<h4 style={{ textAlign: 'center' }}>Автопробег «Бывших не бывает»</h4>
+					</div>
 				</div>
 			</section>
 		</>
